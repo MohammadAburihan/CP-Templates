1e3a333a8b83482c6d5b81b32fb5f85cc082817a
maburehan <maburehan@apple.com>
2025-12-25 23:21:28 +0200

    update LCABinaryLifting.cpp

--- LCABinryLifting.cpp
+++ LCABinryLifting.cpp
@@ -1,56 +1,61 @@
-int n, l;
-vector<vector<int>> adj;
+class BinaryLifting {
+public:
+  int n, l;
+  vector<vector<int>> adj;
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
+  int timer;
+  vector<int> tin, tout;
+  vector<vector<int>> up;
 
-void dfs(int v, int p)
-{
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
-    }
-
-    tout[v] = ++timer;
-}
-
-bool is_ancestor(int u, int v)
-{
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
-}
-
-int lca(int u, int v)
-{
-    if (is_ancestor(u, v))
-        return u;
-    if (is_ancestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
-            u = up[u][i];
-    }
-    return up[u][0];
-}
-
-void preprocess(int root) {
+  BinaryLifting(int n, vector<vector<int>> adj) {
+    this->n = n;
     tin.resize(n);
     tout.resize(n);
     timer = 0;
     l = ceil(log2(n));
     up.assign(n, vector<int>(l + 1));
+    this->adj = adj;
+  }
+
+  void dfs(int v, int p) {
+    tin[v] = ++timer;
+    up[v][0] = p;
+    for (int i = 1; i <= l; ++i)
+      up[v][i] = up[up[v][i - 1]][i - 1];
+
+    for (int u : adj[v]) {
+      if (u != p)
+        dfs(u, v);
+    }
+
+    tout[v] = ++timer;
+  }
+
+  bool is_ancestor(int u, int v) {
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+  }
+
+  int lca(int u, int v) {
+    if (is_ancestor(u, v))
+      return u;
+    if (is_ancestor(v, u))
+      return v;
+    for (int i = l; i >= 0; --i) {
+      if (!is_ancestor(up[u][i], v))
+        u = up[u][i];
+    }
+    return up[u][0];
+  }
+
+  void preprocess(int root) {
     dfs(root, root);
-}
+  }
+};
 
 /*
-*
-*
-* LCA with Binary Lifting Implementaion
-*
-*
-*/
+ *
+ *
+ * LCA with Binary Lifting Implementaion
+ *
+ *
+ */
